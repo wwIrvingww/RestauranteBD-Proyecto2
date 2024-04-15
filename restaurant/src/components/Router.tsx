822fae0457d2c7010b5b88186e38d83a599e5179
wwIrvingww <mor22781@uvg.edu.gt>
2024-04-15 09:31:21 -0600

    bebidas route added

--- restaurant/src/components/Router.tsx
+++ restaurant/src/components/Router.tsx
@@ -2,6 +2,7 @@ import './ExploreContainer.css';
 import Parairving from '../Views/Parairving';
 import Asignacion from '../Views/Asignacion';
 import Reportes from '../Views/Reportes';
+import Bebidas from '../Views/Bebidas';
 
 interface ContainerProps {
   name: string;
@@ -20,6 +21,11 @@ const Router: React.FC<ContainerProps> = ({ name }) => {
   } else if (name == 'Reportes'){
     return (
       <Reportes />
+    )
+  }
+  else if (name == 'Bebidas'){
+    return(
+      <Bebidas />
     )
   }
 };
